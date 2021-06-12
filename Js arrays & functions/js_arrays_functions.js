31bbc6647d41d9f601738ff5e7396086fc835b04
DCtron <deepakchoudhary1026@gmail.com>
2021-06-12 14:17:22 +0530

    commit

--- Js arrays & functions/js_arrays_functions.js
+++ Js arrays & functions/js_arrays_functions.js
@@ -37,3 +37,21 @@ myColor = ["Red", "Green", "White", "Black"];
 console.log(myColor.toString());
 console.log(myColor.join());
 console.log(myColor.join('+'));
+
+//5
+var arr1 = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+var mf = 1;
+var m = 0;
+var item;
+for (var i = 0; i < arr1.length; i++) {
+    for (var j = i; j < arr1.length; j++) {
+        if (arr1[i] == arr1[j])
+            m++;
+        if (mf < m) {
+            mf = m;
+            item = arr1[i];
+        }
+    }
+    m = 0;
+}
+console.log(item + " ( " + mf + " times ) ");
