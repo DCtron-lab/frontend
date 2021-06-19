3f09dbe7f65ad15226cecd170ac292e40d51a67b
DCtron <deepakchoudhary1026@gmail.com>
2021-06-19 14:41:11 +0530

    changes

--- seat booking/seat.js
+++ seat booking/seat.js
@@ -38,3 +38,30 @@ document.addEventListener("click", (e) => {
         remaining.innerHTML = ` ${rcount}`;
     }
 });
+
+
+
+
+/* let bookedSeatsElement= document.querySelector('.booking_seats');
+let remainingSeatsElement = document.querySelector('.remaining_seats');
+let seats= document.querySelectorAll('.seats');
+
+let bookSeats =0;
+let remainingSeats = seats.length;
+
+bookedSeatsElement.innerText = bookedSeats;
+remainingSeatsElement.innerText = remainingSeats;
+ seats.forEach((seat)=> {
+    seat.addEventListener('click',() => {
+        seat.classList.add('booked');
+        seat.classList.toggle("booked");
+        seat.classList.contains('booked')? bookedSeats(1) : unbookseats(-1);
+    });
+ });
+
+ const bookSeats = (s) => {
+    bookedSeats +=1*s;
+    remainingSeats -=1*s;
+
+    bookedSeatsElement.innerText = bookedSeats;
+    remainingSeatsElement.innerText = remainingSeats; */
