017b3a9d77aa2bb92025e083880dc17ff126b349
DCtron <deepakchoudhary1026@gmail.com>
2021-07-11 20:49:28 +0530

    commit

---
+++ memecard/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "card" >
+        <
+        div className = "image" >
+        <
+        img src = "https://i.pinimg.com/736x/dc/62/ba/dc62ba6433b6e744b095585c50883540.jpg"
+        alt = "meme" >
+        <
+        /img> < /
+        div > <
+        h1 > Meme card < /h1> <
+        div className = "cardDescription" >
+        <
+        /div> < /
+        div >
+    );
+}
+
+export default App;
