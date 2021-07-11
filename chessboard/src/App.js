70ed9881c115091e5ba5765ab48cb0f7f22aab79
DCtron <deepakchoudhary1026@gmail.com>
2021-07-11 21:00:04 +0530

    commit

---
+++ chessboard/src/App.js
@@ -0,0 +1,100 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "chessboard" >
+        <
+        div className = "row" >
+        <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        /div> <
+        div className = "row" >
+        <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        /div> <
+        div className = "row" >
+        <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        /div> <
+        div className = "row" >
+        <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        /div> <
+        div className = "row" >
+        <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        /div> <
+        div className = "row" >
+        <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        /div> <
+        div className = "row" >
+        <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        /div> <
+        div className = "row" >
+        <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        div className = "block" > < /div> <
+        /div> <
+        /div>
+    );
+}
+
+export default App;
